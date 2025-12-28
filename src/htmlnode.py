7f5ab0635d3c9756e2d8fd1122e5540b4590ee1e
gsedory99 <gsedory99@gmail.com>
2025-12-28 09:41:45 -0500

    htmlnode added with tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,11 +9,33 @@ class HTML_Node:
         raise NotImplementedError
 
     def props_to_html(self):
-        formatted_string = ""
-        if self.props:
-            for key, value in self.props.items():
-                formatted_string += f' {key}="{value}"'
-        return formatted_string
+        if not self.props:
+            return ""
+        return "".join([f' {key}="{val}"' for key, val in self.props.items()])
 
     def __repr__(self):
         return f"tag ={self.tag}, value={self.value},children={self.children}, props={self.props}"
+
+
+class LeafNode(HTML_Node):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError
+        if not self.tag:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTML_Node):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag or not self.children:
+            raise ValueError
+        children_string = "".join(node.to_html() for node in self.children)
+        return f"<{self.tag}{self.props_to_html()}>{children_string}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTML_Node
+from htmlnode import HTML_Node, LeafNode, ParentNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -15,6 +15,24 @@ class TestTextNode(unittest.TestCase):
     def test_props_to_html3(self):
         node = HTML_Node(props={"href": "www.google.com", "target": "_blank"})
         self.assertEqual(node.props_to_html(), ' href="www.google.com" target="_blank"')
+
+    def test_leaf_to_html(self):
+        node = LeafNode("p", "www.google.com", {"target": "_blank"})
+        print(node.to_html())
+        self.assertEqual(node.to_html(), '<p target="_blank">www.google.com</p>')
+
+    def test_to_hetml_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_hetml_with_children2(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(), "<div><span><b>grandchild</b></span></div>"
+        )
 
 
 if __name__ == "__main__":
