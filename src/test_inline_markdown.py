aab41447dd36211bb501dc986ef7cd5d2f51c3ff
Grayson Sedory <Gsedory99@gmail.com>
2026-01-25 23:38:50 -0500

    name change

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,12 +1,12 @@
 import unittest
-from convert_textnode import split_nodes_delimeter
+from inline_markdown import split_nodes_delimiter
 from textnode import TextNode, TextType
 
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_delim_bold_simple(self):
         node = TextNode("This is **bold** text", TextType.PLAIN_TEXT)
-        new_nodes = split_nodes_delimeter([node], "**", TextType.BOLD_TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
         self.assertListEqual(
             [
                 TextNode("This is ", TextType.PLAIN_TEXT),
