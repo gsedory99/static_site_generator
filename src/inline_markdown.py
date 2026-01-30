449a6084f24f9376abb920f3fdc6ba842a8afa04
Grayson Sedory <Gsedory99@gmail.com>
2026-01-29 20:41:25 -0500

    inlinemarkdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from inline_markdown import extract_markdown_images, extract_markdown_links
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -18,3 +19,18 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 new_nodes.append(TextNode(split_text_list[i], text_type))
     return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN_TEXT:
+            new_nodes.append(node)
+            continue
+        image_info = extract_markdown_images(node.text)
+        sections = node.text.split(f"![{image_info[0]}]({image_info[1]})", 1)
+        text_node_build = []
+
+
+def split_nodes_link(old_nodes):
+    pass
