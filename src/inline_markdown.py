426d4cf8ede9d7ce22f9a4a6af7f4370bce98bae
Grayson Sedory <Gsedory99@gmail.com>
2026-02-02 22:43:18 -0500

    inline_markdown_start

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -27,9 +27,31 @@ def split_nodes_image(old_nodes):
         if node.text_type != TextType.PLAIN_TEXT:
             new_nodes.append(node)
             continue
-        image_info = extract_markdown_images(node.text)
-        sections = node.text.split(f"![{image_info[0]}]({image_info[1]})", 1)
-        text_node_build = []
+
+        original_text = node.text
+        images = extract_markdown_images(original_text)
+
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+
+        for image_alt, image_link in images:
+            sections = original_text.split(f"![{image_alt}]({image_link})", 1)
+
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.PLAIN_TEXT))
+
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+
+            original_text = sections[1]
+
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.PLAIN_TEXT))
+
+    return new_nodes
 
 
 def split_nodes_link(old_nodes):
