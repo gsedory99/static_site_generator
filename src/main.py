a7db42eb09fe1f1c7de13909edc0cb2d8f51cb53
Grayson Sedory <Gsedory99@gmail.com>
2026-02-24 17:35:48 -0500

    Restructure project to use docs directory and add build script
    This commit moves the output directory from 'public' to 'docs', updates
    the scripts to reflect this change, and adds a new build.sh script for
    easier site generation. It also removes unnecessary entries from
    .gitignore and includes initial HTML

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
+import sys
+
 from copy_to_public import copy_static
 from generate_page import generate_pages_recursive
 
 
 def main():  # Test the new text_to_textnodes function
-
-    copy_static("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    copy_static("static", "docs")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+        generate_pages_recursive(
+            basepath + "content", "template.html", basepath + "docs"
+        )
+    generate_pages_recursive("content", "template.html", "docs")
 
 
 if __name__ == "__main__":
